c9957bad2c687c1060efd6ff93850b330308e823
PRIYANSH-2004 <priyanshsingh.pr@gmail.com>
2025-03-28 02:57:57 +0530

    reports has been created

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import Home from "./app/screens/Home";
 import Reports from "./app/screens/Reports";
 import PairDevice from "./app/screens/PairDevice";
 import FileTransfer from "./app/screens/FileTransfer";
+import DetailedReport from "./app/screens/DetailedReport";
+import TempDownload from "./app/screens/TempDownload";
 
 // Create the stack navigator
 const Stack = createNativeStackNavigator();
@@ -17,6 +19,8 @@ export default function App() {
         <Stack.Screen name="Reports" component={Reports} />
         <Stack.Screen name="Pair Device" component={PairDevice} />
         <Stack.Screen name="File Transfer" component={FileTransfer} />
+        <Stack.Screen name="Detailed Report" component={DetailedReport} />
+        <Stack.Screen name="Temp Download" component={TempDownload} />
       </Stack.Navigator>
     </NavigationContainer>
   );
